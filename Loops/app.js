ec7b653b7ea27849faecbfbfa856503b93ddc0fe
ANUMARIF <155381975+anumarif833@users.noreply.github.com>
2024-09-21 02:16:50 +0500

    Add files via upload
    dun

---
+++ Loops/app.js
@@ -0,0 +1,157 @@
+// for (let i = 1; i <= 10; i++){
+// document.write(`2 x ${i} = ${2*i}<br><br>`);
+// } =0, i<=city
+
+
+// for(var i=0; i<=10; i++){
+//     document.write (i,")  i love you too <br>")
+// };
+
+// 
+
+// var city = ["karachi","islamabad","lahor"]
+// document.write(city[0],"<br>")
+// document.write(city[1],"<br>")
+// document.write(city[2],"<br>")
+
+
+
+
+// var city = ["karachi", "lahor", "pisha"]
+// var userCity = prompt("enter your city:")
+// var matchFound = "no";
+// // document.write(city
+
+
+// for(var i = 0, i<city.langht; i++){
+
+// }
+// if(city[i]===userCity){
+   
+//     matchFound=true
+//     alert("City found")
+//     break;
+//       }
+// if(matchFound=="no"){
+//     alert("city not found")
+// }
+
+// for (i=i; i<=10; i++){
+//     document.write(`2 x ${i} = ${i*2} </br>`);
+//     document.write(`2 x ${i} = ${i*2} </br>`);
+// }
+
+// var userinput = +prompt("Enter your table Number");
+// for (num=1; num<=10; num++)[
+//     document.writeln(`${userinput} x ${num} = ${userinput*num} </br>`)
+// ];
+
+// var tablelNumber = +prompt("Enter the your number for table");
+// var minlenght = +prompt("Enter minimum langht");
+// var maxlanght = +prompt("Enter maxlanght langt");
+
+// for(let i= minlenght; i<= maxlanght; minlenght++){
+//     document.write(`${tablelNumber} x ${minlenght} = ${tablelNumber*minlenght}`);
+// }
+// for (let i = 1; i <= 10; i++){
+//     document.write(`3 x ${i} = ${3*i}<br><br>`);
+//     }
+
+// var city = ["karachi","lahor","islamabad"]
+// var userCity = prompt("Enter yor city name");
+// var fjageValu = falas;
+// for (city[i]= userCity({ 
+//     flagValu = userCity
+// }
+
+// var restruent = ["kabab","achargosht","tandori chiken","biryani"]
+// var menue= prompt("what you want to like");
+// var somedish = false;
+// for(let i=0; i<restruent.length; i++){
+// if(resturant [i]===menue){
+//     someDish=true
+//     alert("Thank you");
+//     break;
+// }
+// }
+// if(someDish==false){
+
+//     alert("not fond")
+//     document.write("<h1>" +"Menue"+ "</h1>")
+//     for(j=0; j<restruent.length; j++)
+//     document.write(restruent[j],"<br>")
+// }
+
+// for ( var i=0; i<=5; i++){
+//     for (var j=1; j<=5; j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+
+// }
+
+// for ( var i=0; i<=5; i++){
+//     for(var j=1; j<=i; j++){
+//         document.write("*")
+//         }
+//      document.write("<br>")
+//     }
+
+    // for ( var i=1; i<=5; i++){
+    //     for (var j=5; j>=i; j--){
+    //         document.write("*")
+    //     }
+    //     document.write("<br>")
+    // }
+
+
+// var n= +prompt("Enter Number of rows:");
+// for (i=1;i<=n; i++){
+// for(j=1; j<=i; j++){
+//     document.write(" "+ j)
+// }
+// document.write("<br>")
+// }
+
+// document.write("<br>");
+// for(i=1;i<=n; j++){
+//     for(j=1; j<=n; j++){
+//         document.write(" "+ j)
+//     }
+//     document.write("<br>")
+// }
+
+// document.write("<br>")
+
+// for(i=1;i<=n; i++){
+//     for(j=n; j>=1; j--){
+//         document.write(" "+ j)
+//     }
+//  document.write("<br>");
+//  }
+
+
+
+// var studName= ["sara","sana","zara",]
+// var FatherName=['Ali',"Sanaullah","Asif",]
+// for(i=0 ;i<studName,length;i++){
+// for(j=0 ;j<Father_Name,length;j++){
+// document.write(studName,length[i] + FatherName_Name[j] +"<br>");
+// }
+    
+// }
+    
+// var multidimension=[["sara"[10],"zara"[10],"sana"[12],]]
+// document.write(multidimension[2],[12])
+// // for(i=0 ;i<studName,length;i++){
+// //  for(j=0 ;j<Father_Name,length;j++){
+// //  document.write(studName,length[i] + FatherName_Name[j] +"<br>");
+// // }
+    
+// var array=[[1,2,3],[4,5,6],[7,8,9],];
+// for (var i=0;i<array.length;i++){
+//     for (var j=0;j<array.length[i];j++){
+//   document.write(arry[i][j])
+//     }
+//     document.write("<br>")
+// }
